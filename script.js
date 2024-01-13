8d8d5de0af1dfa3c8f73086d21a7f720125e53bc
VASPerera <avishka1999perera@gmail.com>
2024-01-13 23:13:22 +0530

    js file

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 'use strict';
 
 const score0 = document.querySelector('#score--0');
+const score1 = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.game');
+const btnRoll = document.querySelector('.roll-dice');
+const btnHold = document.querySelector('.hold');
+
+
+ diceEl.classList.remove('hidden');
+ score.textContent = 'hgs';
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function(){
+
+    // 1.genarating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+
+    //2. Display Dice
+    diceEl.classList.remove('.hidden');
+    diceEl.src = 'images/dice${dice}.png';
+
+
+    //3. check for rolled 1: if switch to next player
+
+});
+
+
+
