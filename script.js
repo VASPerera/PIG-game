e2c6ed3b6f7c408ea77bf61cad62cdffa9e0c8cb
VASPerera <avishka1999perera@gmail.com>
2024-01-14 00:33:09 +0530

    day 1

--- script.js
+++ script.js
@@ -2,28 +2,40 @@
 
 const score0 = document.querySelector('#score--0');
 const score1 = document.getElementById('score--1');
+const currentScore0El = document.getElementById('currentscore--0');
+const currentScore1El = document.getElementById('currentscore--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.game');
 const btnRoll = document.querySelector('.roll-dice');
 const btnHold = document.querySelector('.hold');
 
+let currentScore = 0;
 
- diceEl.classList.remove('hidden');
- score0.textContent = 'hgs';
+
+ diceEl.classList.add('hidden');
+
 
 //rolling dice functionality
 btnRoll.addEventListener('click', function(){
 
+    
     // 1.genarating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
 
     //2. Display Dice
-    diceEl.classList.remove('.hidden');
-    diceEl.src = 'images/dice${dice}.png';
+    diceEl.classList.remove('hidden');
+    diceEl.src = 'images/dice-' + dice + '.png';
+    
 
 
     //3. check for rolled 1: if switch to next player
+    if(dice!==1){
+        currentScore = currentScore + dice;
+        currentScore0El.textContent = currentScore;
+    }
 
 });
 
